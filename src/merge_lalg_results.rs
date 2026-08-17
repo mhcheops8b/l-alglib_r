ae27ff2522b7ba7fe5f6fc37a0187d673bf1ae4a
Marek Hyčko <mhycko@gmail.com>
2026-08-17 09:24:29 +0200

    Adding option into `merge_lalg_results` utility.

--- src/merge_lalg_results.rs
+++ src/merge_lalg_results.rs
@@ -1,12 +1,48 @@
 use std::collections::HashSet;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 use std::fs::File;
 
+// fn main() {
+//     let input=String::from("-");
+//     let mut res_file = BufReader::new(&std::io::Read);
+//     if input == "-" {
+//         res_file = BufReader::new(stdin().lock() as dyn std::io::Read);
+//     }
+//     else {
+//         res_file = BufReader::new(File::open(input).expect("Cannot open `ord_filename` file") as dyn std::io::Read);
+//     }
+//     return;
+//     // let res_file = std::io::stdin();
+//     
+
+//     for res_line in res_file.lines() {
+//         let cur_result = serde_json::from_str::<Vec<Vec<usize>>>(&res_line.unwrap()).unwrap();
+//         if all_results.insert(cur_result.clone()) {
+//             println!("{:?}", cur_result);
+//         }    
+//     }
+// }
+
 fn main() {
+    let args_len = std::env::args().len();
 
-    let res_file = std::io::stdin();
+    if args_len < 2 {
+        println!("Usage: {} <input filename | - (for stdin)>", std::env::args().next().unwrap());
+        return;
+    }
+
+    let filename = std::env::args().nth(1).unwrap();
+
+    if filename == "-" {
+        merge_fun(BufReader::new(stdin().lock()));
+    }
+    else {
+        merge_fun(BufReader::new(File::open(filename).expect("Cannot open `ord_filename` file")));
+    }    
+}
+
+fn merge_fun<T:std::io::Read>(res_file: BufReader<T>) {
     let mut all_results = HashSet::<Vec<Vec<usize>>>::new();
-
     for res_line in res_file.lines() {
         let cur_result = serde_json::from_str::<Vec<Vec<usize>>>(&res_line.unwrap()).unwrap();
         if all_results.insert(cur_result.clone()) {
